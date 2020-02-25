67a45e756e5ce3f1627b2c1eb79271e98562da9b
Wire Wolf <apple.link.level@gmail.com>
2020-02-24 22:57:58 -0700

    -

--- buildahfile-Scratch.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-echo "Starting Buildah Scratch Build"
-set -x
-#Intended to run on Fedora
-rose=$(buildah from scratch)
-rosemnt=$(buildah mount $rose)
-dnf install --installroot $scratchmnt --releasever 30 python python-pip --setopt install_weak_defs=false -y
-buildah run "$rose" -- echo "EMPTY"
